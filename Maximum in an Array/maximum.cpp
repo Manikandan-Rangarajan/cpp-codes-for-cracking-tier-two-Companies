3ae6db4b7cdcbae1692b540d3f063978b2a185ba
Manikandan-Rangarajan <116093831+Manikandan-Rangarajan@users.noreply.github.com>
2024-08-26 11:49:37 +0530

    Create maximum.cpp

---
+++ Maximum in an Array/maximum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void maximum(int arr[],int n){
+    int i=0,max=arr[0];
+    for(i;i<n;i++){
+        if(max>arr[i]){
+            max = max;
+        }else{
+            max = arr[i];
+        }
+    }
+    cout<<"The Maximum of the given array is: "<<max<<endl;
+}
+
+int main() {
+    int n;
+    cout<<"Enter Limit of Array"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<=n;i++){
+        cin>>arr[i];
+    }
+    maximum(arr,n);
+    return 0;
+}
